5f2fe00c5d4dab89d8cfb32ce67b5935a49fbad6
Nicole Wong <nicolew0410@hstat.org>
2021-03-08 15:46:07 +0000

    update repo

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 $.ajax({
-    url: 'https://api.publicapis.org/entries',
+    url: 'https://data.cityofnewyork.us/resource/25th-nujf.json',
     dataType: 'json',
     success: function(data) {
         console.log(data);
     }
 });
+// Later: HTML/CSS introduce topic
+var theirName = prompt("What is your name?")
+for (var i = 1; i < theirName; i++) {
+    if (theirName == "nm"){
+        document.write("There are other people with that name!") // ignore for now
+    }
+
+var i = 0
+for (let i in "nm") {
+   console.log(i + theirName[i])
+}
+
 
 // data set/api: https://data.cityofnewyork.us/resource/25th-nujf.json
 // search enginge: find out if your name is popular
