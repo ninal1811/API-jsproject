5bfb873618f745f0a69bd079e957e9ccc18f36da
Nina Li <ninal1811@hstat.org>
2021-03-12 02:57:38 +0000

    update repo

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ $.ajax({
     url: 'https://data.cityofnewyork.us/resource/25th-nujf.json',
     dataType: 'json',
     success: function(data) {
-      
         function everything(){
         var theirName = prompt("What is your name?");
         for (var i = 0; i < 1000; i++){
             if (theirName == data[i].nm){
                 var count = data[i].cnt;
-                console.log(count);
             }
         }
         var math = parseInt(count)
@@ -25,19 +23,26 @@ $.ajax({
         //     console.log("Okay Thanks!")
         // }
         // extension(); 
-        
-        var keepRecommending = true;
-        while(keepRecommending){
-          alert("Please write 'yes' or 'no' for the following question.")//Specification on answers in the loop.
-          var userAnswer = prompt("Are you having a female baby and you need a name idea?");
-          if (userAnswer == "yes"){
-            console.log(data)
-          }
-          else if(userAnswer == "no"){
-            keepRecommending = false;
-            alert("Okay, thank you for coming!")
-          }
+        function extension(){
+            alert("Please write 'yes' or 'no' for the following question.");//Specification on answers in the loop.
+            var userAnswer = prompt("Are you having a female baby and you need a name idea?");
+            if (userAnswer == 'yes'){
+                for (var i = 0; data.length; i++){
+                document.write(data.cnt);   
+                }
+                
+            }
         }
+        extension();
+        //for (var i = 0; i < 1000; i++){
+            //if (userAnswer == 'yes'){
+                //document.write(data);
+            //}
+        //} //else if(userAnswer == "no"){
+            //keepRecommending = false;
+            //alert("Okay, thank you for coming!");
+          //}
+        //}
         
     }
 });
